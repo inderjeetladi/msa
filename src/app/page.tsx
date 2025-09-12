d251bb9cf2e0a39f2dd1ccbde4a6f0247da58a44
inderjeetladi <inderjeetladi@rediff.com>
2025-09-12 18:47:33 +0530

    Fixed double quotes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,7 +103,7 @@ export default function Home() {
                     <svg className="w-5 h-5 text-blue-500 mr-2" fill="currentColor" viewBox="0 0 20 20">
                       <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
                     </svg>
-                    <span className="text-gray-700">Rainfall forecast: 2.8" over next 5 days</span>
+                    <span className="text-gray-700">{'Rainfall forecast: 2.8" over next 5 days'}</span>
                   </div>
                   <div className="flex items-center">
                     <svg className="w-5 h-5 text-orange-500 mr-2" fill="currentColor" viewBox="0 0 20 20">
